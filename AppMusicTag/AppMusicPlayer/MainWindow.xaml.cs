6396ee64719a3c9a4c5e32192b671ad65d51e008
David Chipana <chipana_david@hotmail.com>
2020-07-23 20:26:10 -0300

    First commit

---
+++ AppMusicTag/AppMusicPlayer/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace AppMusicPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Select_Click(object sender, RoutedEventArgs e)
+        {
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                var tagFile = TagLib.File.Create(openFileDialog.FileName);
+                foreach (string x in tagFile.Tag.Performers)
+                {
+                    Console.WriteLine("Artista: {0}", x);
+                }
+                grid.DataContext = tagFile;
+                if (tagFile.Tag.Pictures.Length > 0)
+                {
+                    MemoryStream ms = new MemoryStream(tagFile.Tag.Pictures[0].Data.Data);
+                    ms.Seek(0, SeekOrigin.Begin);
+                    BitmapImage bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.StreamSource = ms;
+                    bitmap.EndInit();
+                    albumImage.Source = bitmap;
+
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ AppMusicTag/AppMusicPlayer/Musica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMusicPlayer
+{
+    public class Musica
+    {
+        public string Title;
+        public string Path;
+        public Musica()
+        {
+
+        }
+        public Musica(string title, string path)
+        {
+            this.Title = title;
+            this.Path = path;
+        }
+    }
+}
